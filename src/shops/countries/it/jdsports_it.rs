67ee4163bad860b612b3b03e6da8782f67b56f49
Pricy <pricyproject@proton.me>
2023-02-19 08:46:16 -0500

    Refactor jdsports codes

--- src/shops/countries/it/jdsports_it.rs
+++ src/shops/countries/it/jdsports_it.rs
@@ -6,14 +6,15 @@ use crate::{
         create_sitemap::create_local_sitemap,
         website::{get_response, get_sitemap_links_by_content},
     },
+    DYNAMIC_ARGS,
 };
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use async_trait::async_trait;
 use heck::ToSnakeCase;
 use select::{document::Document, predicate::Attr};
 
 use chrono::Utc;
-use std::{fmt::Display, fs::read_to_string, io::Write};
+use std::{fmt::Display, fs::read_to_string};
 #[derive(Debug)]
 pub struct JdsportsIt;
 
@@ -22,8 +23,8 @@ impl JdsportsIt {
         Self {}
     }
     pub async fn crawl_single_url(valid_url: &str) -> Result<()> {
-        let m = valid_url.to_string();
-        println!("{m:?}");
+        let product = crawl_url(valid_url).await?;
+        println!("{}", serde_json::to_string_pretty(&product)?);
         Ok(())
     }
 }
@@ -32,6 +33,39 @@ impl Display for JdsportsIt {
         write!(f, "{:?}", &self)
     }
 }
+
+async fn crawl_url(url: &str) -> Result<Product> {
+    let product_page_content = get_response(url).await?;
+    let mut product = Product {
+        ..Default::default()
+    };
+
+    if !product_page_content.contains("Nessun prodotto corrisponde alla tua ricerca.")
+        && product_page_content.contains(DYNAMIC_ARGS.filter_keyword.as_str())
+        && url.contains(DYNAMIC_ARGS.filter_url.as_str())
+    {
+        let document = Document::from(product_page_content.as_str());
+
+        for node in document.find(Attr("type", "application/ld+json")).take(1) {
+            let result = node.text();
+            let json_result: serde_json::Value = serde_json::from_str(&result)?;
+            product.link = url.to_string();
+            product.image = format!("{}.jpg", json_result["image"][0].as_str().unwrap());
+            product.name = json_result["name"].as_str().unwrap().to_string();
+            product.currency = Currency::Euro;
+            product.price = json_result["offers"]["price"]
+                .to_string()
+                .parse::<f64>()
+                .unwrap();
+            product.description = json_result["description"].as_str().unwrap().to_string();
+            product.brand = json_result["brand"]["name"].as_str().unwrap().to_string();
+            product.color = json_result["color"].as_str().unwrap().to_string();
+        }
+    }
+
+    Ok(product)
+}
+
 #[async_trait]
 impl Shop for JdsportsIt {
     async fn crawl(&self) -> Result<()> {
@@ -58,32 +92,9 @@ impl Shop for JdsportsIt {
 
         let mut products: Vec<Product> = Vec::new();
         for link in sitemap_file_content.lines().take(5) {
-            let product_page = get_response(link).await?;
+            let product = crawl_url(link).await?;
 
-            if !product_page.contains("Nessun prodotto corrisponde alla tua ricerca.") {
-                let document = Document::from(product_page.as_str());
-                let mut product = Product {
-                    ..Default::default()
-                };
-
-                for node in document.find(Attr("type", "application/ld+json")).take(1) {
-                    let result = node.text();
-                    let json_result: serde_json::Value = serde_json::from_str(&result)?;
-                    product.link = link.to_string();
-                    product.image = format!("{}.jpg", json_result["image"][0].as_str().unwrap());
-                    product.name = json_result["name"].as_str().unwrap().to_string();
-                    product.currency = Currency::Euro;
-                    product.price = json_result["offers"]["price"]
-                        .to_string()
-                        .parse::<f64>()
-                        .unwrap();
-                    product.description = json_result["description"].as_str().unwrap().to_string();
-                    product.brand = json_result["brand"]["name"].as_str().unwrap().to_string();
-                    product.color = json_result["color"].as_str().unwrap().to_string();
-                }
-
-                products.push(product);
-            }
+            products.push(product);
         }
         let crawl_result = CrawlOutput {
             shop: shop_detail.clone(),
